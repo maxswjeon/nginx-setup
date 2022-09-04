815780195130f2b5549fd3ba627184a1de820a7f
Jeon Sangwan <maxswjeon@naver.com>
2022-09-04 15:25:20 +0000

    Create sites-available and sites-enabled only if does not exist

--- bootstrap.sh
+++ bootstrap.sh
@@ -40,5 +40,10 @@ scripts/nginx_genconf ssl "${DEFAULT_HOST}"
 cp templates/nginx.conf nginx.conf
 sed -i "s/{{ DEFAULT_HOST }}/${DEFAULT_HOST}/g" nginx.conf
 
-mkdir sites-available
-mkdir sites-enabled
+if [ ! -d sites-available ]; then
+	mkdir sites-available
+fi
+
+if [ ! -d sites-enabled ]; then
+	mkdir sites-enabled
+fi
